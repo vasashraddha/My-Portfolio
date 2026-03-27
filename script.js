a3c88af663b0f03d02e6120159d35d6e28074902
SHRADDHA VASA <55424438+vasashraddha@users.noreply.github.com>
2026-03-27 18:50:46 +0530

    Add typing animation and project filter functions

---
+++ script.js
@@ -0,0 +1,31 @@
+// Typing Animation
+let text = "AI/ML Engineer | Time Series Expert 🚀";
+let i = 0;
+
+function typing() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 50);
+    }
+}
+typing();
+
+
+// Filter Function
+function filterProjects(category) {
+    let cards = document.getElementsByClassName("card");
+
+    for (let card of cards) {
+
+        if (category === "all") {
+            card.style.display = "block";
+        } else {
+            if (card.classList.contains(category)) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        }
+    }
+}
