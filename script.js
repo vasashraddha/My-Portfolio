dca834c9759806137c003eeb89a37dce930a5db2
SHRADDHA VASA <55424438+vasashraddha@users.noreply.github.com>
2026-03-28 13:29:05 +0530

    Modify text variable declaration and typing speed
    Changed 'const' to 'let' for text variable and adjusted typing speed.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const text = "AI/ML Engineer | Time Series Specialist 🚀";
+let text = "AI/ML Engineer | Time Series Specialist 🚀";
 let i = 0;
 
 function typing() {
     if (i < text.length) {
         document.getElementById("typing").innerHTML += text.charAt(i);
         i++;
-        setTimeout(typing, 80);
+        setTimeout(typing, 50);
     }
 }
 window.onload = typing;
